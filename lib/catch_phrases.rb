78b5e1e2c44ba7745dc3820fbd9baf5d02e7a71f
Jeff Swisher <jefftswisher@gmail.com>
2019-09-22 04:22:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,10 +3,13 @@ def mario
 end
 
 def toadstool
-  puts status = "Thank you Mario! But our Princess Is In Another Castle!"
+  puts phrase = "Thank you Mario! But our Princess Is In Another Castle!"
 end
 
 def link
   puts phrase = "It's Dangerous To Go Alone! Take This."
 end
   
+  def any_phrase
+    puts phrase = "Do A Barrel Roll!"
+  end
