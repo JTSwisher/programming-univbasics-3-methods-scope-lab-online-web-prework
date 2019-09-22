2562fbcfbce920a0c3e079e10de57356df85bdff
Jeff Swisher <jefftswisher@gmail.com>
2019-09-22 04:26:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,4 @@
 def mario
-  puts phrase = "It's-a me, Mario!"
-end
-
-def toadstool
-  puts phrase = "Thank you Mario! But our Princess Is In Another Castle!"
-end
-
-def link
-  puts phrase = "It's Dangerous To Go Alone! Take This."
-end
-  
-def any_phrase
-  puts phrase = "Do A Barrel Roll!"  
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
